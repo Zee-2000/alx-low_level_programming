370e8cf07e3919f11539c7313f86f03064cb7873
Zee-2000 <zahwaabdelnasser@gmail.com>
2023-03-04 00:58:07 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,6 +10,7 @@ int main(void)
 {
 unsigned long fib1 = 0, fib2 = 1, fibsum;
 float tot_sum;
+
 while (1)
 {
 fibsum = fib1 + fib2;
@@ -20,7 +21,6 @@ if ((fibsum % 2) == 0)
 tot_sum += fibsum;
 
 fib1 = fib2;
-		
 fib2 = fibsum;
 }
 printf("%.0f\n", tot_sum);
