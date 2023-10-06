e86ce93a9270c6e669674f5dc133193572444e82
Zee-2000 <zahwaabdelnasser@gmail.com>
2023-10-07 01:11:57 +0300

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ printf("Last digit of %d is %d and is zero\n", n, n % 10);
 }
 else
 {
-printf("Lastt digit od %d is %d and is less than 5\n", n, n % 10);
+printf("Last digit of %d is %d and is less than 5\n", n, n % 10);
 }
 return (0);
 }
