cbc85ed651ff764dbb4e3b63fcdd31572dafdc93
Zee-2000 <zahwaabdelnasser@gmail.com>
2023-10-08 12:18:09 +0300

    Reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char o;
+for (o = 'z'; o >= 'a'; o--)
+{
+putchar(o);
+}
+putchar('\n');
+return (0);
+}
