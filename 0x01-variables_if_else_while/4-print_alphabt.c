29e2c37284feee061ebe27b6bbfde70d7e40c1b2
Zee-2000 <zahwaabdelnasser@gmail.com>
2023-10-08 11:46:26 +0300

    Except Q and E

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+
+if (c != 'q' && c != 'z')
+{
+
+putchar(c);
+}
+
+putchar('\n');
+
+}
+return (0);
+}
+
