fa7eabb09ea154e2f9c0e037821aed32d0b295b4
Zee-2000 <zahwaabdelnasser@gmail.com>
2023-10-25 11:33:21 +0300

    Most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints
+ * Owned by Bwave ICT
+ * Return void
+ */
+
+
+void print_most_numbers(void)
+{
+char x;
+for (x = '0'; x <= '9'; x++)
+if (x != '2' && x != '4')
+{
+_putchar(x);
+}
+putchar('\n');
+}
+
