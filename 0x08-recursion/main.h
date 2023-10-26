f95584dcd2e0d230c673fc4cbca70dec58d80799
Zee-2000 <zahwaabdelnasser@gmail.com>
2023-10-27 01:37:10 +0200

    sqrt

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,6 +6,8 @@ void _print_rev_recursion(char *s);
 int strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int check(int a, int b);
 
 
 #endif
