57109ca3abd9b5c9c68ee3e55dcedf3811a7ff52
Zahwa A. El Nasser <104075923+Zee-2000@users.noreply.github.com>
2024-04-03 19:49:25 +0000

    pointers strings arrays

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,21 +8,14 @@
 char *_strchr(char *s, char c)
 {
   int i;
-  for(i = 0; s[i] != '\0'; i++)
-    {
-      if(s[i] == c)
-      {
-        return(s + i);
-        
-      }
-     
-    }
-  if(s[i] == c)
+
+  for (i = 0; s[i] != '\0'; i++)
   {
+    if (s[i] == c)
+      return (s + i);
+  }
+  if (s[i] == c)
     return (s + i);
-  }
   else
-  {
     return ('\0');
-  }
 }
